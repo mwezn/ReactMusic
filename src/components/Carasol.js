b9fda8ee9bf43cff6a670b2eb151a88e2a164215
Michael Nelson <michael.nelson@getfutureproof.co.uk>
2022-04-06 15:26:22 +0100

    changes

---
+++ src/components/Carasol.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
+
+import img1 from '../assets/c&s.png'
+import img2 from '../assets/cx.jpg'
+import img3 from '../assets/gc.jpg'
+
+
+
+class Cards extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state= {autoplay :false, buttonVal:"play"}
+    }
+    
+    pausePlay(){
+        this.state.autoplay?this.setState({autoplay :false, buttonVal:"play"}):this.setState({autoplay:true,buttonVal:"X"})
+        
+    }
+    
+    render() {
+     
+      const btnStyle={width:"75px",height:"75px", position:"relative", top:"0px",backgroundColor:"rgb(1,1,1,0.7)",color:"white"}
+      
+      let slides2=[
+         <img  src={img1} alt="1" />,
+         <img  src={img2} alt="2" />  ,
+         <img  src={img3} alt="3" />  ]
+      return (
+        <div>
+            <h1>React-responsive-carousel</h1>
+        
+        <div id="section2">
+        <button style={btnStyle} onClick={(e)=>this.pausePlay()}>{this.state.buttonVal}</button>
+        <Carousel width="50%" height="50%" statusFormatter={(current, total) => `${current} / ${total}`} showThumbs={false} infiniteLoop={true} interval={3000} autoPlay={this.state.autoplay}>
+        {slides2}
+        </Carousel>
+        </div>
+        </div>
+      );
+    }
+  }
+
+  
+
+  export default Cards;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,34 @@
+import React from "react";
+import './intro.css'
+
+import img1 from '../assets/c&s.png'
+import img2 from '../assets/cx.jpg'
+import img3 from '../assets/gc.jpg'
+
+
+
+const MainPage =()=>{
+    let artists=[
+        {name: "Chronixx", genre:"Reggae", img:img2},
+        {name: "Gorgon City", genre:"House/dance",img:img3},
+        {name: "Chase & Status", genre:"Drum and Bass",img:img1},
+        {name: "The Weeknd", genre:"Pop"}
+    ]
+    let result=artists.map((d,i)=>{
+        return (
+        <div className={'card'+(i+1)}>
+            <h1>{d.name}</h1><p>{d.genre}</p><img src={d.img}/>
+        </div>)
+    })
+    
+    return(
+        <div className="intro">
+            <h1>My favourite music</h1>
+            <div className="artists">
+                {result}
+            </div>
+        </div>
+    )
+}
+
+export default MainPage;
